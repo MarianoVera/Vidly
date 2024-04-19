b19c1b6086851e7701fb2ca1c8f3acaf2efedb4e
Mariano <mariano.vera98@hotmail.com>
2024-04-18 23:08:58 -0300

    Get started

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+    }
+}
