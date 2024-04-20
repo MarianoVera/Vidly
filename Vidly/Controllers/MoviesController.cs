8f48899453c1b812968e37a2b25cda821131a71c
Mariano <mariano.vera98@hotmail.com>
2024-04-20 17:00:11 -0300

    adding RandomViewModel and PartialView to make code clean

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,15 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+        //[Route("movies/random")]
         public IActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+
+            var customers = new List<Customer>
+            {
+                new Customer{ Name = "Customer 1"},
+                new Customer{ Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
